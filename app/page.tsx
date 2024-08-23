85e84e1113a5e2a3233ae226d9e5c9daf411d9d7
Ebraam <Ebraam@gmail.com>
2024-08-23 03:38:05 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Editor from '@/components/Editor';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='dark:bg-slate-800 bg-slate-300 p-8'>
+      <Editor />
+      </div>
+  )
+}
